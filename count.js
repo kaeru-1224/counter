ecca87a2e4ffa168f82f9ac6c7664ea25ede2b2f
JISU <cjfslzpf@naver.com>
2021-07-30 16:21:42 +0900

    Update count.js

--- count.js
+++ count.js
@@ -7,8 +7,7 @@ let newnum= 0;
 function chnageColor(){
     if(newnum>0) {number.style.color='red';}
     else if (newnum==0){number.style.color='black';}
-  else {number.style.color='blue';} 
-}
+  else {number.style.color='blue';} }
 
 
 
@@ -16,10 +15,10 @@ function increaseNumber() {
     newnum+=1;
     number.innerText=newnum;
     chnageColor()}
-function resetNumber(){  newnum=0;  number.innerText=newnum;chnageColor()}
-function decreasNumber(){newnum-=1;number.innerText=newnum;chnageColor()
-}   
-
+function resetNumber(){  
+        newnum=0;  number.innerText=newnum;chnageColor()}
+function decreasNumber(){
+        newnum-=1;number.innerText=newnum;chnageColor()}   
 
 up.addEventListener('click', increaseNumber)
 reset.addEventListener('click',resetNumber)
